cde05f2169174317a82e2168ab11dd5b62f7b366
Peeratchai Kleebbua <p-ratchai@hotmail.com>
2023-09-06 03:26:39 +0700

    tableReview

--- client/components/BenchmarkReview/TableBenchmarkReview/index.tsx
+++ client/components/BenchmarkReview/TableBenchmarkReview/index.tsx
@@ -8,7 +8,6 @@ import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -46,12 +45,27 @@ const rows = [
   createData("Eclair", 262, 16.0, 24, 6.0),
   createData("Cupcake", 305, 3.7, 67, 4.3),
   createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
 ];
 
 const TableBenchmarkReview = () => {
   return (
-    <TableContainer >
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+    <TableContainer sx={{ maxHeight: 577 }} >
+      <Table
+        stickyHeader
+        sx={{ minWidth: 700 }}
+        aria-label="benchmark review table"
+      >
+        <caption>A Benchmark Review table for memristor</caption>
         <TableHead>
           <TableRow>
             <StyledTableCell>Dessert (100g serving)</StyledTableCell>
