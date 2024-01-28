fa0096f88bf9c987b53f34192bf71806364ccb1e
Peeratchai Kleebbua <p-ratchai@hotmail.com>
2024-01-29 01:53:02 +0700

    cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: []
-  })
+    origin: ['http://localhost:3000'],
+  });
   await app.listen(3001);
 }
 bootstrap();
